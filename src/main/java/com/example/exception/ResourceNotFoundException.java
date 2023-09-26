9ea47d7143d3d8bc5eeda7ba9c53c30a6f2e23dc
Leon Poyau <leopoyau@gmail.com>
2023-09-26 03:30:57 +0000

    Fixed a Typo in Custom Exception class

--- src/main/java/com/example/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/exception/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package com.example.exception;
 public class ResourceNotFoundException extends RuntimeException{
     /* Added this custom exception class for show, but it's not been used
      * in this project. 
+     * 
     */
     public ResourceNotFoundException(String message) {
 		super(message);
